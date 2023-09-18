d28273151aa94bb22ffeef9725386bf936d8acf3
Hunter Evans <moab@flibit.com>
2023-09-17 23:44:46 -0600

    Add files via upload

---
+++ SWDV140/BoxesPlus/js/script.js
@@ -0,0 +1,16 @@
+/*********************************************************************
+***
+*Original Author:                                   Hunter Evans
+*Date Created:                                      17 September 2023
+*Version:                                           2
+*Date Last Modified:                                17 September 2023
+*Modified by:                                       Hunter Evans
+*Modification log:                                  2
+
+    Version 2 - 17 September 2023 - Updated site.
+***
+******************************************************************** */
+
+
+"use strict";
+
