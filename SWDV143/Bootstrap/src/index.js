43c1a32ba72297cb1b3f5d808ccc7e70d67d7ca4
Hunter Evans <moab@flibit.com>
2023-12-15 18:07:49 -0700

    Add files via upload

---
+++ SWDV143/Bootstrap/src/index.js
@@ -0,0 +1,8 @@
+import './styles.scss';
+
+// const myCarouselElement = document.querySelector('#carousel');
+
+// const carousel = new bootstrap.Carousel(myCarouselElement, {
+//   interval: 2000,
+//   touch: false,
+// });
