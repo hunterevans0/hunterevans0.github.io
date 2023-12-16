42420cd5c686ee79f91de66e5189f30f3602f284
Hunter Evans <moab@flibit.com>
2023-12-15 18:06:24 -0700

    Delete SWDV143/Bootstrap directory

--- SWDV143/Bootstrap/src/index.js
+++
@@ -1,8 +0,0 @@
-import './styles.scss';
-
-// const myCarouselElement = document.querySelector('#carousel');
-
-// const carousel = new bootstrap.Carousel(myCarouselElement, {
-//   interval: 2000,
-//   touch: false,
-// });
